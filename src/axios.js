d9b54719769e4160ba48b07ac102df19a617d13d
AliHafeez337 <alihafeez337@gmail.com>
2020-04-02 16:28:24 +0500

    5th - same as 4th with a minor change which i forgot...

--- src/axios.js
+++ src/axios.js
@@ -13,10 +13,10 @@ axios.defaults.baseURL = 'http://localhost:3000'
 // And I don't know the way to check if the header is set or not (for universal)
 // So the best solutoin is to set the header before you confirm it in the action.js file..
 
-// Getters.getToken().then((token) => {
-//   console.log(token)
-//   axios.defaults.headers.common['x-auth'] = token
-// })
+getters.getToken().then((token) => {
+  console.log(token)
+  axios.defaults.headers.common['x-auth'] = token
+})
 // axios.defaults.headers.common['x-auth'] = localStorage.getItem('user-token')
 
 axios.interceptors.request.use(config => {
