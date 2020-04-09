10c163e9a35f62ea42182ac792a28746bf6623b5
omergr8 <umar.mukhtar4455@gmail.com>
2020-04-10 02:30:12 +0500

    some issues fixed

--- src/views/ui-elements/data-list/list-view/download.vue
+++ src/views/ui-elements/data-list/list-view/download.vue
@@ -364,9 +364,9 @@
             <div
               class="vx-card p-2"
               v-if="
-                activeUserInfo.usertype === user ||
-                  activeUserInfo.usertype === customer ||
-                  activeUserInfo.usertype === partner ||
+                activeUserInfo.usertype === downloads.userAllowed[0] ||
+                    activeUserInfo.usertype === downloads.userAllowed[1] ||
+                    activeUserInfo.usertype === downloads.userAllowed[2] ||
                   activeUserInfo.usertype === 'admin'
               "
             >
@@ -583,8 +583,8 @@ export default {
           })
           .then(resp => {
             this.downloads = resp.data;
-            var keys = Object.keys(resp.data);
-            var rdd = resp.data;
+            // var keys = Object.keys(resp.data);
+            // var rdd = resp.data;
             resolve(resp);
           })
           .catch(err => {
@@ -607,10 +607,10 @@ export default {
           this.downloads = resp.data;
           var keys = Object.keys(resp.data);
           var rdd = resp.data;
-          for (var i = 0; i < keys.length; i++) {
-            var value = rdd[i];
-            var ua = value.userAllowed;
-          }
+          // for (var i = 0; i < keys.length; i++) {
+          //   var value = rdd[i];
+          //   var ua = value.userAllowed;
+          // }
         })
         .catch(err => {
           console.log(err);
